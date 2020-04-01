32d7ef88d5075e3b58d062b59a0cee54546d770a
djra26111990 <djra26111990@gmail.com>
2020-04-01 18:50:49 -0500

    Update index.php

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ $thankYou="<p>Gracias! su mensaje ha sido enviado!.</p>";
   <title>Escena compañia de arte y teatro</title>
   </head>
   <body>
-    <section class="section-top">
+    <section class="section section-top">
       <img src="/img/#" alt="Logo de escena">
       <div>
         <h1>Escena</h1>
@@ -41,33 +41,39 @@ $thankYou="<p>Gracias! su mensaje ha sido enviado!.</p>";
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tristique eget est ut facilisis. Nulla quis ligula purus. Etiam dictum rhoncus lorem, eu egestas libero accumsan quis. Donec posuere bibendum nisi a rhoncus.</p>   
       </div>
     </section>
-    <section class="section-play-meet">
-      <div>
-        <iframe width="420" height="315" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
+    <section class="section section-play-meet">
+    <div class="row">    
+      <div class="columns">
+        <iframe width="360" height="275" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
+      </div>
+      <div class="columns">
         <h1>Conoce</h1>
         <h3>Nullam sit amet leo cursus, rhoncus justo vitae, mollis diam. Proin gravida justo eu aliquet aliquet.</h3>
       </div>
+    </div>
     </section>
-    <section class="section-clases">
+    <section class="section section-clases">
       <h1>Clases</h1>
-      <div>
+      <div class ="row">
+      <div class="columns">
         <h2>Niños</h2>
-        <img src="#" alt="Niños">
+        <i class="fas fa-child fa-3x"></i>
         <p>Donec id lorem a justo ultricies malesuada. Aenean commodo sit amet metus gravida molestie. Cras nec nulla rhoncus, lacinia leo at, hendrerit odio.</p>
       </div>
-      <div>
+      <div class="columns">
         <h2>Jóvenes</h2>
-        <img src="#" alt="Jóvenes">
+        <i class="fas fa-user fa-3x"></i>
         <p>Donec id lorem a justo ultricies malesuada. Aenean commodo sit amet metus gravida molestie. Cras nec nulla rhoncus, lacinia leo at, hendrerit odio.</p>
+      </div>
       </div>
     </section>
-    <section class="section-programa">
+    <section class="section section-programa">
       <div>
         <h1>Programa Continuo</h1>
         <p>Nam a feugiat nulla, et interdum leo. Ut imperdiet consequat justo, sed ultrices dui ullamcorper eu. Ut fermentum vulputate libero, sed fringilla mi dapibus eu. In hac habitasse platea dictumst.</p>     
       </div>
     </section>
-    <section class="section-contacto">
+    <section class="section section-contacto">
     <h1>Contáctanos</h1>
     <?=$thankYou ?>
     
@@ -81,16 +87,18 @@ $thankYou="<p>Gracias! su mensaje ha sido enviado!.</p>";
         <label>Message:</label>
         <textarea rows="5" cols="20" name="message"></textarea>
 
-        <input type="submit" name="submit">
+        <input class="btn" type="submit" name="submit">
     </form>
     </section>
-    <footer class="footer">
+    <section class="section footer">
+        <div class="div-footer">
         <h1>Síguenos: </h1>
         <ul>
             <li><a href="#"><i class="fab fa-facebook-square fa-3x"></i></a></li>
             <li><a href="#"><i class="fab fa-instagram fa-3x"></i></a></li>
         </ul>
-    </footer>
+        </div>
+    </section>
     
     <script src="https://cdnjs.cloudflare.com/ajax/libs/rellax/1.12.1/rellax.min.js" integrity="sha256-+xf9aJnHocnmrigq2hIDJGBSAnJdF5NH+Ooe5J2PHiI=" crossorigin="anonymous"></script>
     
